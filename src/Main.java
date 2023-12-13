44e8baad83f29d6d55b3b92a1580df53cec9893d
EgorRom <romantsov@mail.ru>
2023-12-13 13:44:55 +0300

    Home Work 10

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         task1();
-        task2();}
-        public static void task1() {
-            System.out.println("Hello world!");
-            System.out.println("Задание №1");
-            String firstName = "Ivan ";
-            String middleName = "Ivanovich";
-            String lastName = "Ivanov ";
-            String fullName = lastName + firstName + middleName;
-            System.out.println("Ф.И.О. сотрудника - " + fullName);
-            System.out.println("Задание №2");
-            System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
-        }
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Hello world!");
+        System.out.println("Задание №1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println("Задание №2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+    }
+
     public static void task2() {
         System.out.println("Задание №3");
         String fullName = "Иванов Семён Семёнович";
